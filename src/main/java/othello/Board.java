a4f7675fb30996e9b4f5f99bf2b6343afc96ae05
baba <baba@heisei-intelligence.com>
2020-06-03 14:43:31 +0900

    ソースコード途中まで

--- src/main/java/othello/Board.java
+++ src/main/java/othello/Board.java
@@ -1,28 +1,29 @@
 package othello;
 
 public class Board {
-	
+
 	private static Board board = new Board();
-	
+
 	private static final int BOARD_SIZE = 8;
 
 	private Cell[][] cells = new Cell[BOARD_SIZE][BOARD_SIZE];
-	
+
 	static {
-		for(int y =0; y < BOARD_SIZE; ++y) {
-			for(int x = 0; x < BOARD_SIZE;++x) {
+		for(int x = 0; x < BOARD_SIZE; x++) {
+			for (int y = 0; y < BOARD_SIZE; y++) {
 				board.cells[x][y] = new Cell(x,y);
- 			}
+			}
 		}
 	}
-	
+
 	private Board() {
 	}
-	
+
 	public static Cell getCell(int x, int y) {
 		if(x < 0 || x > 7 || y < 0 || y > 7) {
 			return null;
 		}
 		return board.cells[x][y];
+
 	}
 }
--- src/main/java/othello/Cell.java
+++ src/main/java/othello/Cell.java
@@ -6,224 +6,173 @@ public class Cell {
 
 	private int x, y;
 
+
+
 	public Cell(int x, int y) {
-		//0~7の範囲でなければならない
-		if(x < 0 || x > 7 || y < 0 || y > 7) {
-			return;
+		if(x >= 0 && x <=7 && y >= 0 && y <= 7 ) {
+			this.x = x;
+			this.y = y;
+
 		}
-		this.x = x;
-		this.y = y;
 	}
-	//Xの位置
-	public int getX() {
+
+	// xの値を取得
+	public int getX(int x) {
 		return this.x;
 	}
-	//Yの位置
-	public int getY() {
+
+	// yの値を取得
+	public int getY(int y) {
 		return this.y;
 	}
-	//状態
+
+	// 現在の状態を取得
 	public int getStatus() {
 		return this.status;
 	}
 
+
 	public boolean possibleMoveBlack() {
-		if(this.status != 0) {
+		if(this.status == 1 || this.status == 2) {
 			return false;
-		}else{
-			for(int i = -1; i < 2; i++) {
-				for(int z = -1;z < 2; z++ ) {
-					if(i == 0 && z == 0) {
+		}else {
+			for(int i = -1; i <= 1; i++) {
+				for(int j = -1; j <= 1; j++) {
+
+					if(i == 0 && j == 0) {
 						continue;
 					}
-					if(x+i < 0 || x+i > 7 || y+z < 0 || y+z > 7) {
+
+					if(x + i < 0 || x + i > 7 || y + j < 0 || y + j > 7) {
 						continue;
 					}
-					if(Board.getCell((x+i),(y+z)).status == 2) {
-						if(Board.getCell((x+i),(y+z)).canFlip(this) == true) {
-							return true;
-						}
+
+					if(Board.getCell(x+i,y+j).canFiip(this) == true){
+						return true;
 					}
+
 				}
 			}
-			return false;
+
 		}
+		return false;
+
 	}
 
 	public boolean possibleMoveWhite() {
-		if(this.status != 0) {
+		if(this.status == 1 || this.status == 2) {
 			return false;
-		}else{
-			for(int i = -1; i < 2; i++) {
-				for(int z = -1;z < 2; z++ ) {
-					if(i == 0 && z == 0) {
+		}else {
+			for(int i = -1; i <= 1; i++) {
+				for(int j = -1; j <= 1; j++) {
+
+					if(i == 0 && j == 0) {
 						continue;
 					}
-					if(x+i < 0 || x+i > 7 || y+z < 0 || y+z > 7) {
+
+					if(x + i < 0 || x + i > 7 || y + j < 0 || y + j > 7) {
 						continue;
 					}
-					if(Board.getCell((x+i),(y+z)).status == 1) {
-						if(Board.getCell((x+i),(y+z)).canFlip(this) == true) {
-							return true;
-						}
+
+					if(Board.getCell(x+i,y+j).canFiip(this) == true){
+						return true;
 					}
+
 				}
 			}
+
 			return false;
 		}
+
 	}
 
-	boolean canFlip(Cell client) {
-		int X = client.x;
-		int Y = client.y;
-		if(client.x == this.x) {
-			X = this.x;
+	boolean canFiip(Cell client) {
+		int x1 = client.x;
+		int y1 = client.y;
+
+
+		if(this.x == client.x) {
+			x1 = this.x;
+		}else if(this.x < client.x) {
+			x1 = this.x + 1;
+		}else{
+			x1 = this.x -1;
 		}
-		if(client.x < this.x) {
-			X = this.x+1;
+
+		if(this.y == client.y) {
+			y1 = this.y;
+		}else if(this.y < client.y) {
+			y1 = this.y + 1;
+		}else if(this.y > client.y) {
+			y1 = this.y -1;
 		}
-		if(client.x > this.x) {
-			X = this.x-1;
-		}
-		if(client.y == this.y) {
-			Y = this.y;
-		}
-		if(client.y < this.y) {
-			Y = this.y+1;
-		}
-		if(client.y > this.y) {
-			Y = this.y-1;
-		}
-		//自分自身が「なし」の時false
-		if(this.status == 0) {
-			return false;
-		}
-		
-		//呼び出し元が「なし」の時自分自身が引数となりcanFlipを呼び出す
+
 		if(client.status == 0) {
-			if(X < 0 || X > 7 || Y < 0 || Y > 7 ) {
+			return Board.getCell(x1, y1).canFiip(this);
+		}else if(this.status != client.status){
+			return true;
+		}else {
+			if(x1 != client.x || y1 != client.y) {
+				return Board.getCell(x1,  y1).canFiip(this);
+			}else {
 				return false;
 			}
-			return Board.getCell(X,Y).canFlip(this);
 		}
-		
-		
-		if(client.status == this.status) {
-			if(X < 0 || X > 7 || Y < 0 || Y > 7 ) {
-				return false;
-			}else
-				return Board.getCell(X,Y).canFlip(this);
-		}
-		
-		
-		if(client.status != this.status) {
-			return true;
-		}
-		return false;
 	}
 
 	public void onMove(boolean white) {
 		if(white) {
-			for(int i = -1; i < 2; i++) {
-				for(int z = -1;z < 2; z++ ) {
-					if(i == 0 && z == 0) {
+			for(int i = -1; i <= 1; i++) {
+				for(int j = -1; j <= 1; j++) {
+
+					if(i == 0 && j == 0) {
 						continue;
 					}
-					if(x+i < 0 || x+i > 7 || y+z < 0 || y+z > 7) {
+
+					if(x + i < 0 || x + i > 7 || y + j < 0 || y + j > 7) {
 						continue;
 					}
-					if(Board.getCell((x+i),(y+z)).status == 1) {
-						if(Board.getCell((x+i),(y+z)).canFlip(this) == true) {
-							Board.getCell((x+i),(y+z)).flip(this,true);
-						}
+
+					if(Board.getCell(x+i, y+j).canFiip(this) == true) {
+						flip(this, false);
 					}
 				}
 			}
-			this.status = 2;
-		}
-		else {
-			for(int i = -1; i < 2; i++) {
-				for(int z = -1;z < 2; z++ ) {
-					if(i == 0 && z == 0) {
+		}else {
+			for(int i = -1; i <= 1; i++) {
+				for(int j = -1; j <= 1; j++) {
+
+					if(i == 0 && j == 0) {
 						continue;
 					}
-					if(x+i < 0 || x+i > 7 || y+z < 0 || y+z > 7) {
+
+					if(x + i < 0 || x + i > 7 || y + j < 0 || y + j > 7) {
 						continue;
 					}
-					if(Board.getCell((x+i),(y+z)).status == 2) {
-						if(Board.getCell((x+i),(y+z)).canFlip(this) == true) {
-							Board.getCell((x+i),(y+z)).flip(this,false);
-						}
+
+					if(Board.getCell(x+i, y+j).canFiip(this) == true) {
+						flip(this, false);
 					}
 				}
 			}
-			this.status = 1;
 		}
 	}
 
 	public void flip(Cell client, boolean white) {
-		//白
 		if(white && this.status == 2) {
-			return;
-		}else if(white && this.status == 1) {
-			this.status = 2;
-			int X = client.x;
-			int Y = client.y;
-			if(client.x == this.x) {
-				X = this.x;
-			}
-			if(client.x < this.x) {
-				X = this.x+1;
-			}
-			if(client.x > this.x) {
-				X = this.x-1;
-			}
-			if(client.y == this.y) {
-				Y = this.y;
-			}
-			if(client.y < this.y) {
-				Y = this.y+1;
-			}
-			if(client.y > this.y) {
-				Y = this.y-1;
-			}
-			if(X < 0 || X > 7 || Y < 0 || Y > 7 ) {
-				throw new RuntimeException();
-			}else {
-				Board.getCell(X,Y).flip(this,true);
-			}
-		}
 
-		//黒
-		if(!white && this.status == 1) {
-			return;
+		}else if(white && this.status == 1) {
+			if(canFiip(client)) {
+				flip(client, true);
+			}else {
+				//例外発生
+			}
+
+		}
+		if(!white && this.status ==1) {
+
 		}else if(!white && this.status == 2) {
 			this.status = 1;
-			int X = client.x;
-			int Y = client.y;
-			if(client.x == this.x) {
-				X = this.x;
-			}
-			if(client.x < this.x) {
-				X = this.x+1;
-			}
-			if(client.x > this.x) {
-				X = this.x-1;
-			}
-			if(client.y == this.y) {
-				Y = this.y;
-			}
-			if(client.y < this.y) {
-				Y = this.y+1;
-			}
-			if(client.y > this.y) {
-				Y = this.y-1;
-			}
-			if(X < 0 || X > 7 || Y < 0 || Y > 7 ) {
-				throw new RuntimeException();
-			}else {
-				Board.getCell(X,Y).flip(this,false);
-			}
 		}
 	}
 
